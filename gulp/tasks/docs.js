9bb88320e3c4269e4862aaa04d1a42d249c66a3f
Volga <vtikunov@yandex.ru>
2019-03-10 23:46:15 +0300

    docs: Создание русскоязычной версии

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -6,7 +6,7 @@ var panini = require('panini');
 var supercollider = require('supercollider');
 
 var PANINI_CONFIG = {
-  root: 'docs/pages/',
+  root: 'docs/pages/ru/',
   layouts: 'docs/layout/',
   partials: ['docs/partials/', 'node_modules/foundation-docs/templates/partials/'],
   helpers: foundationDocs.handlebarsHelpers,
